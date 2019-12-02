fe78c35913ef60bdd51e4f9498f9a26eaa04e5da
Sagar Jaybhay <sagarjaybhay18091988@gmail.com>
2019-12-02 18:01:45 +0530

    Added 2 repositories

---
+++ Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tutorial.Model;
+
+namespace asprazorpage.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IEmployeeRepo _employeeRepo;
+
+        public IEnumerable<ClsEmployee> _employeeList;
+
+        public IndexModel(IEmployeeRepo employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+
+        public void OnGet()
+        {
+            _employeeList = _employeeRepo.GetAllEmployees();
+        }
+    }
+}
